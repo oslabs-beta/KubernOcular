f0dd506b83b8bb17dc19af089365a69a6df51ae0
emenegger <evanemenegger@gmail.com>
2022-09-26 09:41:20 -0500

    add dashboard with functioning charts, but hardcoded

---
+++ client/Components/ClusterGraphs/GarbageCollection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FC } from 'react';
+
+const GarbageCollection: FC = () => {
+  return (
+    <div>
+      Garbage Collection Chart
+    </div>
+  )
+};
+
+export default GarbageCollection;
