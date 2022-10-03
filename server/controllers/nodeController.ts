3444988c7b9d44a328703748a3c2fd2bac92b42c
shirley-luu <sh.rleyluu@gmail.com>
2022-10-03 12:23:23 -0400

    backend for general cluster data

--- server/controllers/nodeController.ts
+++ server/controllers/nodeController.ts
@@ -1,4 +1,4 @@
-import { start, end, NodeController, fetch, axios } from '../../types';
+import { start, end, NodeController, axios } from '../../types';
 
 
 const nodeController: NodeController = {
@@ -6,13 +6,13 @@ const nodeController: NodeController = {
     getInstantNetworkTransmitBytes: (req, res, next) => {
         const { node } = req.query;
         //node_network_transmit_bytes_total
-        fetch(`http://localhost:9090/api/v1/query`)
+        // fetch(`http://localhost:9090/api/v1/query`)
     },
 
     getInstantNetworkRecievedBytes: (req, res, next) => {
         const { node } = req.query;
         //node_network_receive_bytes_total
-        fetch(`http://localhost:9090/api/v1/query`)
+        // fetch(`http://localhost:9090/api/v1/query`)
     }
 
 }
