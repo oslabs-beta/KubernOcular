b6aa4d461c712d7470411218697956fb528fd851
Adam Selph <adam@selph.net>
2022-10-06 11:59:21 -0400

    add hierarchy to frontend

--- client/Components/VisualizerTab.tsx
+++ client/Components/VisualizerTab.tsx
@@ -1,8 +1,20 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import CytoscapeComponent from 'react-cytoscapejs';
 
 const VisualizerTab = () => {
+    const [elements, setElements] = useState([]); 
+
+    useEffect(() => {
+        fetch('/api/hierarchy')
+          .then(response => response.json())
+          .then(data => setElements(data))
+    },[])
+
     return (
-        <h1>Visualizer</h1>
+        <div>
+            <h1>Visualizer Tab</h1>
+            <CytoscapeComponent elements={elements} style={ { width: '100%', height: '1080px', border: 'solid' } }></CytoscapeComponent>
+        </div>
     )
   }
   
