a29fe2eb6eb28dc70dd448a473436ef30e4ed85b
MichaelMezhiritskiy <mezhmichael@gmail.com>
2022-10-08 12:30:08 -0400

    visualizer legender draft

--- client/Components/VisualizerTab.tsx
+++ client/Components/VisualizerTab.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CytoscapeComponent from 'react-cytoscapejs';
+import colors from '../colors';
 
 const VisualizerTab = () => {
     const [elements, setElements] = useState([]); 
@@ -24,7 +25,7 @@ const VisualizerTab = () => {
                 "border-width": "3px",
                 "border-color": "black",
                 shape: 'rectangle',
-                "background-color": 'yellow'
+                "background-color": colors.solid.yellow,
             }
         },
         {
@@ -33,7 +34,7 @@ const VisualizerTab = () => {
                 "border-width": "3px",
                 "border-color": "black",
                 shape: 'circle',
-                "background-color": 'red',
+                "background-color": colors.solid.pink,
             }
         },
         {
@@ -42,7 +43,7 @@ const VisualizerTab = () => {
                 "border-width": "3px",
                 "border-color": "black",
                 shape: 'triangle',
-                "background-color": 'green'
+                "background-color": colors.solid.green,
             }
         },
         {
@@ -51,14 +52,39 @@ const VisualizerTab = () => {
                 "border-width": "3px",
                 "border-color": "black",
                 shape: 'hexagon',
-                "background-color": 'blue'
+                "background-color": colors.solid.cyan,
             }
-        } 
+        },
+        {
+            selector: "edge",
+            style: {
+              width: 3,
+              "line-color": colors.solid.comment,
+            }
+          } 
+        //"#AAD8FF"
     ]
 
     return (
-        <div>
-            <h1>Visualizer Tab</h1>
+        <div id="visualizer-page-container">
+            <div id="visualizer-legend">
+                <div className='legend-item' id='services'>
+                    <h2 className ='legend-header'>Services</h2> 
+                    <div className='shape'>x</div>
+                </div>
+                <div className='legend-item' id='pods'>
+                    <h2 className ='legend-header'>Pods</h2> 
+                    <div className='shape'>y</div>
+                </div>
+                <div className='legend-item' id='namespaces'>
+                    <h2 className ='legend-header'>Namespaces</h2> 
+                    <div className='shape'>z</div>
+                </div>
+                <div className='legend-item' id='nodes'>
+                    <h2 className ='legend-header'>Nodes</h2> 
+                    <div className='shape'>a</div>
+                </div>
+            </div>
             <CytoscapeComponent elements={elements} stylesheet={styleSheet} style={ { width: '100%', height: '1080px', border: 'solid' } }></CytoscapeComponent>
         </div>
     )
