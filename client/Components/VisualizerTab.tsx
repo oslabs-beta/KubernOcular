06693b9199649fbbe0b99a34086bf66dac94fc53
shirley-luu <sh.rleyluu@gmail.com>
2022-10-09 22:26:58 -0400

    chip and edge edit

--- client/Components/VisualizerTab.tsx
+++ client/Components/VisualizerTab.tsx
@@ -47,7 +47,7 @@ const VisualizerTab = () => {
                 "border-width": "3px",
                 "border-color": "black",
                 shape: 'triangle',
-                "background-color": kubesColors.blue,
+                "background-color": kubesColors.purple,
             }
         },
         {
@@ -63,7 +63,7 @@ const VisualizerTab = () => {
             selector: "edge",
             style: {
               width: 3,
-              "line-color": colors.pastel.translucent.geyser,
+              "line-color": colors.translucent.currentLine,
             }
           } 
         //"#AAD8FF"
@@ -73,24 +73,48 @@ const VisualizerTab = () => {
 // service: colors.pastel.solid.martini
 // node: kubesColors.blue
 // pod: colors.pastel.solid.polo
+
+{/* <Stack direction="row" spacing={1}>
+      <Chip avatar={<Avatar>M</Avatar>} label="Avatar" />
+      <Chip
+        avatar={<Avatar alt="Natacha" src="/static/images/avatar/1.jpg" />}
+        label="Avatar"
+        variant="outlined"
+      />
+    </Stack> */}
+
     return (
         <div id="visualizer-page-container">
             <div>
-            <h1>Visualizer</h1>
-            <Stack direction="row" spacing={1} justifyContent="flex-end" alignItems="center" >
-                
-                <Chip avatar={<Avatar alt="namespace square" src="./namespace.jpg" />} label="Namespaces" variant="outlined" />
-                <Chip avatar={<Avatar>M</Avatar>} label="Nodes" variant="outlined"/>
-                <Chip avatar={<Avatar>M</Avatar>} label="Pods" variant="outlined"/>
+            <Stack direction="row" spacing={1} justifyContent="flex-end" alignItems="center" sx={{ mb: 3 }}>
                 <Chip
-                avatar={<Avatar alt="Natacha" src="/static/images/avatar/1.jpg" />}
+                avatar={<Avatar src="https://hmp.me/dyjo" />}
+                label="Nodes"
+                variant="outlined"
+                style={{ color: kubesColors.purple, borderColor: kubesColors.purple }}
+                />
+                <Chip
+                avatar={<Avatar src="https://hmp.me/dyjn" />}
+                label="Namespaces"
+                variant="outlined"
+                style={{ color: colors.pastel.solid.envy, borderColor: colors.pastel.solid.envy }}
+                />
+                <Chip
+                avatar={<Avatar src="https://hmp.me/dyjh" />}
+                label="Pods"
+                variant="outlined"
+                style={{ color: colors.pastel.solid.polo, borderColor: colors.pastel.solid.polo }}
+                />
+                <Chip
+                avatar={<Avatar src="https://hmp.me/dyjg" />}
                 label="Services"
                 variant="outlined"
+                style={{ color: colors.pastel.solid.martini, borderColor: colors.pastel.solid.martini }}
                 />
             </Stack>
             </div>
             <div style={{display: 'flex', justifyContent: 'center'}}>
-            <CytoscapeComponent elements={elements} stylesheet={styleSheet} style={ { width: '100%', height: '65rem', border: 'solid' } }></CytoscapeComponent>
+            <CytoscapeComponent elements={elements} stylesheet={styleSheet} style={{ width: '100%', height: '65rem', border: 'solid' }}></CytoscapeComponent>
             </div>
         </div>
     )
